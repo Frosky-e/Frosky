ac1a554c194042ef55b4c5a3e4a8d26c67c4ecaa
Frosky <divanshk478@gmail.com>
2026-05-17 14:26:54 +0530

    new change

--- netlify/functions/views.js
+++ netlify/functions/views.js
@@ -1,29 +1,25 @@
-const { getStore } = require("@netlify/blobs");
+let count = 0;
 
 exports.handler = async (event) => {
-  const store = getStore("views");
-  const current = Number((await store.get("count")) || 0);
-
-  // Get only
+  // GET → sirf count dikhao
   if (event.httpMethod === "GET") {
     return {
       statusCode: 200,
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ count: current }),
+      body: JSON.stringify({ count }),
     };
   }
 
-  // Increase
-  const updated = current + 1;
-  await store.set("count", updated.toString());
+  // POST → count badhao
+  count++;
 
   return {
     statusCode: 200,
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ count: updated }),
+    body: JSON.stringify({ count }),
   };
 };
